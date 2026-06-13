5ca4a4b6709e6b6186adfec077cf54970d14739f
vishal-cse185 <kit28.24bcs185@gmail.com>
2026-06-13 13:52:25 +0530

    Add home endpoint

---
+++ src/main/java/com/vishal/url_shortener/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.vishal.url_shortener.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "URL Shortener Backend is Running Successfully!";
+    }
+}
